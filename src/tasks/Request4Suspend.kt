1f9918fe503529af79b6940beb420de6f7e872aa
mon0mon <mon0mon@naver.com>
2025-02-26 02:29:19 +0900

    ✨ feat - task4 완료

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -3,5 +3,15 @@ package tasks
 import contributors.*
 
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req = req, response = it) }
+        .bodyList()
+
+    return repos.flatMap { repo ->
+        service
+            .getRepoContributors(owner = req.org, repo = repo.name)
+            .also { logUsers(repo = repo, response = it) }
+            .bodyList()
+    }.aggregate()
 }
