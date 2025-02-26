fba9125e988c2624b69e6faf369c8a9b85fbd4ee
mon0mon <mon0mon@naver.com>
2025-02-26 22:59:21 +0900

    ✨ feat - task5 완료

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -1,8 +1,24 @@
 package tasks
 
 import contributors.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req = req, response = it) }
+        .bodyList()
+
+    val deferreds = repos.map { repo ->
+        async {
+            log("starting loading for ${repo.name}")
+            service.getRepoContributors(owner = req.org, repo = repo.name)
+                .also { logUsers(repo = repo, response = it) }
+                .bodyList()
+        }
+    }
+    deferreds.awaitAll().flatten().aggregate()
 }
