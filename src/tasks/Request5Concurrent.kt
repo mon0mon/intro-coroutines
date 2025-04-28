3b8a6a7e0d69948a3e77bc7ef9678fd923385d54
mon0mon <mon0mon@naver.com>
2025-04-28 20:42:52 +0900

    ✨ feat - Structured concurrency - Canceling the loading of contributors 완료

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -1,10 +1,18 @@
 package tasks
 
-import contributors.*
-import kotlinx.coroutines.Dispatchers
+import contributors.GitHubService
+import contributors.RequestData
+import contributors.User
+import contributors.log
+import contributors.logRepos
+import contributors.logUsers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.DurationUnit
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
     val repos = service
@@ -15,6 +23,7 @@ suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData)
     val deferreds = repos.map { repo ->
         async {
             log("starting loading for ${repo.name}")
+            delay(3.seconds)
             service.getRepoContributors(owner = req.org, repo = repo.name)
                 .also { logUsers(repo = repo, response = it) }
                 .bodyList()
--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -2,8 +2,23 @@ package tasks
 
 import contributors.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.async
 import kotlin.coroutines.coroutineContext
 
+@OptIn(DelicateCoroutinesApi::class)
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req = req, response = it) }
+            .bodyList()
+
+        val deferreds = repos.map { repo ->
+            GlobalScope.async {
+                log("starting loading for ${repo.name}")
+                service.getRepoContributors(owner = req.org, repo = repo.name)
+                    .also { logUsers(repo = repo, response = it) }
+                    .bodyList()
+            }
+        }
+        return deferreds.awaitAll().flatten().aggregate()
 }
